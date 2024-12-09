963c1376f85331b3c1ef85d27496e5e04cae8838
Mac <mac@MacBook-Air.local>
2024-12-09 19:32:30 +0200

    DataBases Mysql

--- src/core/settings.py
+++ src/core/settings.py
@@ -99,9 +99,18 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'AndreyCreatorWeb$ai_db',
+        'USER': 'AndreyCreatorWeb',
+        'PASSWORD': 'Andrii1212',
+        # Or an IP Address that your DB is hosted on
+        'HOST': 'AndreyCreatorWebApp.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
     }
 }
 
