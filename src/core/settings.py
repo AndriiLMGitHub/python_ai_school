7a2c846f896c3e53d8c2abf4b02283ea4135d094
Mac <mac@MacBook-Air.local>
2024-12-09 20:54:16 +0200

    Static Files

--- src/core/settings.py
+++ src/core/settings.py
@@ -150,10 +150,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATIC_ROOT = BASE_DIR / 'static'
-STATICFILES_DIRS = (
-    BASE_DIR / 'static',  # let Django find static files in the project directory
-)
+STATIC_ROOT = BASE_DIR / 'static'
+# STATICFILES_DIRS = (
+#     BASE_DIR / 'static',  # let Django find static files in the project directory
+# )
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
