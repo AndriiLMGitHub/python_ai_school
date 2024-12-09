10a3bc151233cdc76ba7dfe6d43edef271f691e1
Mac <mac@MacBook-Air.local>
2024-12-09 22:26:11 +0200

    Fix avatar

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
+from django.views.static import serve
 
 
 handler404 = views.my_handler404
@@ -10,6 +11,10 @@ handler404 = views.my_handler404
 urlpatterns = [
     # Admin route
     path('admin/', admin.site.urls),
+
+    # Root media route
+    re_path(r'^media/(?P<path>.*)$', serve,
+            {'document_root': settings.MEDIA_ROOT}),
 
     # Start page route
     path('', views.start_view, name="index"),
